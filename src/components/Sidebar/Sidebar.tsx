3e7d1a9d6740ad1ac3f07a96e180492fc59df7af
jerom <jeromjomanthara@gmail.com>
2025-06-05 13:19:53 +0530

    perf(sidebar): make item dynamic

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,19 +2,22 @@ import './Sidebar.css'
 
 import KVlogo from "../../assets/kv-logo.png";
 import EmployeeIcon from '../../assets/icon.svg'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
 const Sidebar = () => {
+    const location = useLocation();
+    const locationComponents = location.pathname.split("/")
     return (
         <div className="sidebar">
             <Link to="/employees" style={{textDecoration: "none"}} className='logo'>
                 <img src={KVlogo} alt="kv-logo" className="logo-img" />
             </Link>
             <div className="sidebar-items">
-                <Link to="/employees" style={{textDecoration: "none"}} className="sidebar-item-selected" >
+                <Link to={location.pathname} style={{textDecoration: "none"}} className="sidebar-item-selected" >
                     <img src={EmployeeIcon} alt="employees icon" />
-                    <p>Employee List</p>
+                    {(location.pathname == "/employees") ? <p>Employee List</p> : (location.pathname == "/employees/create") ? <p>Employee Create</p> : (locationComponents[1] == "employees" && locationComponents[3] == "update") ? <p>Employee Update</p> : (locationComponents.length == 3 && locationComponents[1] == "employees") ? <p>Employee Details</p> : ''}
+                    
                 </Link>
             </div>
         </div>
